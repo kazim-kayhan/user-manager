aa0eccc035655d7d4f3d60b10c6ba7c0778e4b1c
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2022-04-12 07:59:16 +0430

    Remove unnecessary react imports

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import classes from "./Button.module.css";
 
 const Button = ({ children, className, type, onClick }) => {
--- src/components/ui/ErrorModal.jsx
+++ src/components/ui/ErrorModal.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
